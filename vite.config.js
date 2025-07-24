2ad027ab21bb10add729cb4a21112294c30fd5b4
hudaaaamn <hudamuhammadnur@mail.ugm.ac.id>
2025-07-24 08:35:39 +0800

    index.php path cahge 3

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,15 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: [
-//                 'resources/css/app.css',
-//                 'resources/js/app.js',
-//             ],
-//             refresh: true,
-//         }),
-//     ],
-// });
-// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public_html/build', // arahkan ke folder yg Anda inginkan
-    },
-    base: '/build/', // sesuai dengan lokasi folder build relatif terhadap public root
 });
+
